58d2b208f322dd590e3b40e321dc2c2dc548ab24
Boluwatife <Boluwatifeoladejo@gmail.com>
2024-07-15 22:15:17 +0100

    #

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 
 import { ShowMoreProps } from "@types";
 import { updateSearchParams } from "@utils";
-import { CustomButton } from "@components";
+import { CustomButton } from "@components"; 
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
